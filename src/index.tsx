0172fd5352ec34193d010bbfdc05991b9bac9df6
Nikita Ryabtsev <riabtsev.nikita@ya.ru>
2022-10-07 21:37:24 +0500

    add react router add pages set routes add mobx create user store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
+import Login from 'pages/Login/Login';
+import Root from 'pages/Root/Root';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Router } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />
+    },
+    {
+        path: '/login',
+        element: <Login />
+    }
+])
+
 root.render(
-  <React.StrictMode>
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+import Login from "./Login/Login";
+import Root from "./Root/Root";
+
+export default { Login, Root }
