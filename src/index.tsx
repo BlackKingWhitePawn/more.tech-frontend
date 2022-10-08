ba941257e044fe5ddcb3896d794b0bafd8e21eae
Nikita Ryabtsev <riabtsev.nikita@ya.ru>
2022-10-09 03:49:58 +0500

    build 0.2.1

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import Root from 'pages/Root/Root';
 import User from 'pages/User/User';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
 import './index.scss'
 
-if (process.env.NODE_ENV === 'development') {
-    const { worker } = require('./mocks/browser')
-    worker.start()
-}
+// if (process.env.NODE_ENV === 'development') {
+//     const { worker } = require('./mocks/browser')
+//     worker.start()
+// }
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -21,7 +21,7 @@ const root = ReactDOM.createRoot(
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root />
+        element: <Root />,
     },
     {
         path: '/login',
@@ -38,15 +38,21 @@ const router = createBrowserRouter([
     {
         path: '/user/:id',
         element: <User />
-    },
-    {
-        path: '*',
-        element: <Root />
     }
 ])
 
 root.render(
     <React.StrictMode>
+        {/* <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Root />}>
+                    <Route path='login' element={<Login />} />
+                    <Route path='creating' element={<Creating />} />
+                    <Route path='registration' element={<Registration />} />
+                    <Route path='user/:id' element={<User />} />
+                </Route>
+            </Routes>
+        </BrowserRouter> */}
         <RouterProvider router={router} />
     </React.StrictMode>
 );
