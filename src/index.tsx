b599b3e2c98cf5a52acba805e2fc3189e8cba581
Nikita Ryabtsev <riabtsev.nikita@ya.ru>
2022-10-08 11:35:33 +0500

    add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
+import { Layout } from 'components';
 import Login from 'pages/Login/Login';
 import Root from 'pages/Root/Root';
 import User from 'pages/User/User';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.scss'
 
 if (process.env.NODE_ENV === 'development') {
     const { worker } = require('./mocks/browser')
